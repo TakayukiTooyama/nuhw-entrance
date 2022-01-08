56796886fd81c9bdd46a706161c0236062b2231c
TakayukiTooyama <t.tooyama107@gmail.com>
2022-01-08 12:47:09 +0900

    firebase v8 → v9 update

--- src/models/users.ts
+++ src/models/users.ts
@@ -1,4 +1,4 @@
-import firebase from 'firebase/app';
+import firebase from 'firebase/compat/app';
 
 /*
   下記の型定義には必要なidが定義されていない型があります。
--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,7 +1,7 @@
-import 'firebase/auth';
-import 'firebase/firestore';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
-import firebase from 'firebase/app';
+import firebase from 'firebase/compat/app';
 
 import { Fuego } from './fuego';
 
